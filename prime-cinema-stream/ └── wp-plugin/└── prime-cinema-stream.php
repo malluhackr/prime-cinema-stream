34d9fd0b75efd63ad645a0b8b18b0401d2c65085
malluhackr <cristi7jjr@gmail.com>
2025-07-06 17:07:33 +0530

    prime-cinema-stream.php

---
+++ prime-cinema-stream/ └── wp-plugin/└── prime-cinema-stream.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Prime Cinema Stream
+Description: A test plugin for WP Pusher.
+Version: 1.0
+Author: malluhackr
+*/
+
+function prime_cinema_notice() {
+    echo '<div class="notice notice-success"><p>✅ Prime Cinema Stream plugin is working!</p></div>';
+}
+add_action('admin_notices', 'prime_cinema_notice');
