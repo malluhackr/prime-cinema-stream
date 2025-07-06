85a784258ed707ebbc18d8602f38e2033044aeec
malluhackr <cristi7jjr@gmail.com>
2025-07-06 15:36:25 +0530

    Update prime-cinema-stream.php

--- prime-cinema-stream.php
+++ prime-cinema-stream.php
@@ -1,7 +1,12 @@
 <?php
 /*
 Plugin Name: Prime Cinema Stream
-Description: A custom plugin to stream movies.
+Description: A custom plugin to stream movies from GitHub.
 Version: 1.0
 Author: malluhackr
 */
+
+function prime_cinema_stream_plugin_loaded() {
+    error_log("✅ Prime Cinema Stream plugin loaded.");
+}
+add_action('plugins_loaded', 'prime_cinema_stream_plugin_loaded');
