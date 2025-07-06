984db9cf84852cc940cbb5047e317dc1692ac34f
malluhackr <cristi7jjr@gmail.com>
2025-07-06 16:08:36 +0530

    Update prime-cinema-stream.php

--- prime-cinema-stream.php
+++ prime-cinema-stream.php
@@ -1,12 +1,1 @@
-<?php
-/*
-Plugin Name: Prime Cinema Stream
-Description: A test plugin for GitHub auto-deploy.
-Version: 1.0
-Author: malluhackr
-*/
 
-function prime_cinema_notice() {
-    echo '<div class="notice notice-success"><p>✅ Prime Cinema Stream Plugin is working!</p></div>';
-}
-add_action('admin_notices', 'prime_cinema_notice');
